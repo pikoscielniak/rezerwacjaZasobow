99f3218de3a9228d045f42d8db09c754b1259b7f
Franciszek Okrzesik <franeko@gmail.com>
2013-08-20 15:12:35 +0200

    list view clean

--- js/controllers/eventsListController.js
+++ js/controllers/eventsListController.js
@@ -5,27 +5,8 @@ angular.module('project.controllers')
         function ($scope, reservations, dhxSchedulerConfig, $rootScope, resources, filterData, $q) {
             "use strict";
 
-            $scope.reservations = [];
-
-            reservations.get().then(function(response){
-                $scope.reservations = response;
-            }, function(){
-
-            });
-
-            $scope.loading = false;
-
             $scope.loadMore = function(page) {
                 return reservations.page(page);
             };
-
-//            debugger;
-//            var childrenHeight;
-//            do {
-//                childrenHeight = 0;
-//                $(".list-view").children().each(function(){
-//                    childrenHeight += $(this).height();
-//                });
-//            } while($(".list-view").height() < childrenHeight);
 
         }]);
